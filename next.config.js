b78bf82fd654ec1d63634be82834618d4d94588e
boos1122 <bmm38836@gmail.com>
2025-09-27 06:25:21 +0700

    fix: ensure next.config.js enables appDir and standalone output

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   experimental: {
-    serverActions: { allowedOrigins: ['*'] }
-  }
+    appDir: true,
+  },
+  output: 'standalone',
 };
+
 module.exports = nextConfig;
