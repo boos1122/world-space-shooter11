eb6b954f12d65725b9759d0a12f1cacd2d0f03a5
boos1122 <bmm38836@gmail.com>
2025-09-27 06:01:24 +0700

    chore: fix Next.js config + /game page + assets path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  experimental: { serverActions: { allowedOrigins: ['*'] } },
+  experimental: {
+    serverActions: { allowedOrigins: ['*'] }
+  }
 };
 module.exports = nextConfig;
